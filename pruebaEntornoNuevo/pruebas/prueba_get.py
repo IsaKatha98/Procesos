8ae8aed1275b8bd8cd71d756af7ea416f0da971e
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-10-09 14:42:49 +0200

    ayuda

--- pruebaEntornoNuevo/pruebas/prueba_get.py
+++ pruebaEntornoNuevo/pruebas/prueba_get.py
@@ -1,6 +1,14 @@
 import requests
-url= "https://jsonplaceholder.typicode.com/todos/10" 
+url= "https://jsonplaceholder.typicode.com/posts" 
 response=requests.get (url)
 
 print ("Código de estado: ", response.status_code)
-print (response.json())
+#print (response.json())
+if (response.status_code==200):
+    dict= response.json()
+
+    for clave in dict:
+        print (clave, ":", dict[clave])
+
+else:
+    print("Ha habido un error")
