04f4ad426dac2823bc5f678bb75a800f4d518abb
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-10-23 13:21:58 +0200

    no hice el commit

--- pruebaEntornoNuevo/pruebaFlask/app.py
+++ pruebaEntornoNuevo/pruebaFlask/app.py
@@ -2,15 +2,25 @@ from flask import *
 
 app= Flask (__name__)
 
+#Las rutas relativas van sin barra
+fichero="pruebaFlask\\countries.json"
+
+#Lectura del fichero.
+def leeFichero():
+    archivo=open(fichero, "r")
+    countries=json.load(archivo)
+    archivo.close()
+    return countries
 
 
-countries=[
-    {"id":1, "name":"thailand", "Capital":"Bangkok", "area":513120},
-    {"id":2, "name":"australia", "Capital":"Canberra", "area":7617930},
-    {"id":3, "name":"Egypt", "Capital":"Cairo", "area":1010408},
-]
+def escribeFichero(countries):
+    archivo=open(fichero, "w")
+    json.dump(countries,archivo)
+    archivo.close
+
 
 def _find_next_id():
+    countries=leeFichero()
     return max(country["id"] for country in countries)+1
 
 @app.route('/')
@@ -22,15 +32,14 @@ def index():
 
 
 @app.get("/countries")
-
-
 def get_countries():
-
+    countries=leeFichero()
     return jsonify(countries)
 
 
 @app.get("/countries/<int:id>")
 def getCountry(id):
+    countries=leeFichero()
     for country in countries:
         if country["id"]==id:
             return country, 200
@@ -38,24 +47,51 @@ def getCountry(id):
 
 @app.post("/countries")
 def addCountry():
+    countries=leeFichero()
+    
     if request.is_json:
         country= request.get_json()
         country["id"]=_find_next_id()
         countries.append(country)
-
+        escribeFichero(countries)
         return country, 201
     
     return {"error":"Request must be JSON"}, 415
 
-@app.put("/countries/>int:id>")
-def modifycountry (id):
-    if requests.is_json:
-        newCountry=request.get_json()
+@app.put ("/countries/<int:id>")
+@app.patch ("/countries/<ind:id>")
+
+def modifyCountry(id):
+    countries=leeFichero()
+    if request.is_json:
+        newCountry =request.get_json()
+
         for country in countries:
-            if country ["id"]==id:
-                country[element]=newCountry[element]
-                return country,200
-    return{"error":"request must be JSON"},415
+            if country["id"]==id:
+                for element in newCountry:
+                    country[element]=newCountry[element]
+
+                escribeFichero(countries)
+                return country, 200
+            return {"error":"Country not found"},404
+        return {"error":"Request must be JSON"}, 415
+
+
+
+@app.delete("/countries/<int:id>")
+
+def deleteCountry(id):
+    countries=leeFichero()
+    for country in countries:
+        if country["id"]==id:
+            countries.remove(country)
+            escribeFichero(countries)
+
+            return "{}",200
+    
+    return {"error":"Country not found"},404
+
+
 
 
     
