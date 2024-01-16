0049b7eb457a70b67d07f7f4c3cec2deedab0097
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2024-01-16 11:44:17 +0100

    bloqueos

--- Tema3/boletin1/ejercicio02/hilos.py
+++ Tema3/boletin1/ejercicio02/hilos.py
@@ -1,18 +1,22 @@
 
-import threading
+from threading import *
 from typing import Any
 
-class Contador (threading.Thread):
+class Contador (Thread):
     #atributo
     contador=0
+    l= Lock()
 
     #esto es como un constructor de la clase
     def __init__(self, nombre):
-      threading.Thread.__init__(self, name=nombre)
+      Thread.__init__(self, name=nombre)
 
     def run(self):
+        print ("Ejecutando: ", self.name)
+        Contador.l.acquire()
         while Contador.contador<1000:
             Contador.contador=Contador.contador+1
             print(Contador.contador)
+        Contador.l.release()
        
           
